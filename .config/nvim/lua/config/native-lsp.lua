9ece7532e50f5774bb00fdeb4c855226e4e516e5
mulpdev <mulpdev@gmail.com>
2025-10-04 19:55:14 -0500

    mode changed?

--- .config/nvim/lua/config/native-lsp.lua
+++ .config/nvim/lua/config/native-lsp.lua
@@ -1,16 +1,35 @@
 -- :checkhealth vim.lsp
-
 -- see config examples at https://github.com/neovim/nvim-lspconfig
-
+--
 -- argument 1 to vim.lsp.enable is filename w/o .lua from ../../lsp/
-vim.lsp.enable("asm-lsp")				-- gas, nasm, vmsm
-vim.lsp.enable("autotools_ls")			-- GNU autoconf, autotools, make
-vim.lsp.enable("bashls")				-- bash, sh
-vim.lsp.enable("clangd")				-- c, c++
-vim.lsp.enable("pyright")			    -- python
-vim.lsp.enable("basedpyright")	        -- python
-vim.lsp.enable("cmake")					-- CMake
-vim.lsp.enable("docker_compose_language_service")	-- Docker
-vim.lsp.enable("lua_ls")				-- Lua
-vim.lsp.enable("tsserver")              -- Javascript, TypeScript
-vim.lsp.enable("vscode_html")			-- html, css, embedded JS?
+
+-- gas, nasm, vmsm
+vim.lsp.enable("asm-lsp")
+
+-- GNU autoconf, autotools, make
+vim.lsp.enable("autotools_ls")
+
+-- bash, sh
+vim.lsp.enable("bashls")
+
+-- C, C++
+vim.lsp.enable("clangd")
+
+-- Python
+--vim.lsp.enable("pyright")
+vim.lsp.enable("basedpyright")
+
+-- CMake
+vim.lsp.enable("cmake")
+
+-- Docker
+vim.lsp.enable("docker_compose_language_service")
+
+-- Lua
+vim.lsp.enable("lua_ls")
+
+-- Javascript, Typescript
+vim.lsp.enable("tsserver")
+
+-- HTML, CSS, embedded JS?
+vim.lsp.enable("vscode_html")
